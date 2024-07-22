892c915ff51d2f7a834787751cca141889d89b07
murph9 <murph9y@gmail.com>
2024-07-22 19:53:13 +1000

    put hide menu into document ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,9 @@ $(document).ready(function() {
   $('#sidemenuCollapse').on('click', function() {
     $('#sidemenu').toggleClass('hide');
   });
+  // hide menu by default
+  var mq = window.matchMedia("(max-width: 800px)");
+  if (mq.matches) {
+    $('#sidemenu').toggleClass('hide');
+  }
 });
-
-// hide menu by default
-var mq = window.matchMedia("(max-width: 800px)");
-if (mq.matches) {
-  $('#sidemenu').toggleClass('hide');
-}
